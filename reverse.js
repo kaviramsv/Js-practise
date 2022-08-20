2188c46002a729b7b246d028462332ccd21ac42c
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-08-20 13:50:04 -0400

    Add rev

--- reverse.js
+++ reverse.js
@@ -1,22 +1,17 @@
-const reverse = (sentence) =>{
+const reverse = (sentence) => {
 
   let sentenceArray = sentence.split(" ");
-
   let final = [];
 
-  for(let word of sentenceArray){
- 
+  for (let word of sentenceArray) {
     let reversed = " ";
-  
-    for(let i = word.length-1 ; i >= 0; i--){
-       reversed +=word[i];  
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversed += word[i];
     }
     final.push(reversed);
-
-  }   
-  
-  console.log(final.join(" "));
-  
   }
-  
-  reverse("Welcome to this Javascript Guide!");
+  console.log(final.join(" "));
+}
+
+reverse("Welcome to this Javascript Guide!");
+//emocleW  ot  siht  tpircsavaJ  !ediuG
