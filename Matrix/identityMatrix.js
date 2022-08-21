0ce5fbf3c2cdd6e12ab763763585828e6570c4d5
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-08-21 14:02:18 -0400

    Add modified way for identity matrix

--- Matrix/identityMatrix.js
+++ Matrix/identityMatrix.js
@@ -1,10 +1,25 @@
+//better way
+const identical = (matrix) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if ((i !== j && matrix[i][j] !== 0) || (i === j && matrix[i][j] !== 1)) {
+        return "not identical";
+      }
+    }
+  }
+  return "identical";
+}
+console.log(identical([[1, 0, 0], [2, 1, 0], [0, 0, 1]]))
+
+
+
 const identity = (matrix) => {
   let flag = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      let rows = matrix.length ;
+      let rows = matrix.length;
       let cols = matrix[i].length;
-      if(cols!==rows){
+      if (cols !== rows) {
         return "matrix need to be of same length";
       }
     }
@@ -22,14 +37,14 @@ const identity = (matrix) => {
       }
     }
   }
-  if( flag===0){
+  if (flag === 0) {
     return "identical";
-  }else{
+  } else {
     return "non identical"
   }
 
 }
 
 console.log(identity([[1, 0, 0, 2], [0, 1, 0], [0, 0, 1]]));
-console.log(identity([[1, 0, 0],  [0, 1, 0],  [0, 0, 1]]))
-console.log(identity([[1, 0, 1],  [0, 1, 0],  [0, 0, 1]])) 
+console.log(identity([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+console.log(identity([[1, 0, 1], [0, 1, 0], [0, 0, 1]])) 
