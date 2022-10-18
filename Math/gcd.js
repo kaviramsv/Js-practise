15e5a25be86cbde0e1fba354c4677857df915a8f
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-10-17 20:29:23 -0400

    Gcd

--- Math/gcd.js
+++ Math/gcd.js
@@ -1,12 +1,24 @@
 // gcd/hcf
 
-const gcd = (n1,n2)=>{
+// const gcd = (n1,n2)=>{
+//   let hcf = 1;
+//   for(let i=2;i<n1 && i<n2 ; i++ ){
+//     if(n1%i ===0 && n2%i===0){
+//       hcf=i;
+//     }
+//   }
+//   return hcf;
+// }
+// console.log(gcd(60,72));
+
+const gcd = (n1, n2) => {
   let hcf = 1;
-  for(let i=2;i<n1 && i<n2 ; i++ ){
-    if(n1%i ===0 && n2%i===0){
-      hcf=i;
+  for (let i = 0; i < n1, i < n2; i++) {
+    if (n1 % i === 0 && n2 % i === 0) {
+      hcf = i;
     }
   }
+  console.log(hcf)
   return hcf;
 }
-console.log(gcd(60,72));
+gcd(60, 72);
