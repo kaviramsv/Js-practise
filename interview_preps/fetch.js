87642866744af625bdee7a37ee8e86fe583dd343
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-10-18 09:12:42 -0400

    fetch

--- interview_preps/fetch.js
+++ interview_preps/fetch.js
@@ -1,5 +1,5 @@
-// const fetch = require("node-fetch");
-import fetch from 'node-fetch'
+const fetch = require("node-fetch");
+// import fetch from 'node-fetch'
 // globalThis.fetch = fetch
 const fetchPromise =
   fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
